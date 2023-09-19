4aa3c47ae9379afbbb75675ad2983ce682b49fdf
Fabiola <suzuyajuuzou007@gmail.com>
2023-09-18 21:44:30 -0300

    first commit

---
+++ src/Navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+function(){
+    return{
+        <ul>
+        <li><a href="default.asp">Nome</a></li>
+        <li><a href="news.asp">Projetos</a></li>
+        <li><a href="https://github.com/Fabi417">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/fabiola-maria-braga-dos-santos-88b562212/">Linkedln</a></li>
+        <li><a href="https://www.instagram.com/fabi_mbraga/?next=%2F">Instagram</a></li>
+        </ul>
+      };
+}
